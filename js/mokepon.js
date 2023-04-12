7e94b4b79d63ec1f59e7165af36d448bb0b8afa4
Kevin <kambetacode@gmail.com>
2023-04-12 15:26:34 -0500

    correcion de error en el codigo de mascota aleatoria javascript

--- js/mokepon.js
+++ js/mokepon.js
@@ -127,7 +127,7 @@ function seleccionarMascotaJugador() {
 function seleccionarMascotaEnemigo() {
     let mascotaAleatoria = aleatorio(0,mokepones.length -1)
 
-    spanMascotaEnemigo = mokepones[mascotaAleatoria]
+    spanMascotaEnemigo.innerHTML = mokepones[mascotaAleatoria].nombre
 }
 
 function ataqueFuego() {
